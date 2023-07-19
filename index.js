df1c5f38a703aa28f178a48b236d7975ae71e276
BillQuayle <wjquayle@gmail.com>
2023-07-18 23:20:16 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+
+  if (ride <= 400){
+    return "This one is on me!";
+  }
+  else if (ride < 2000){
+    return 'That will be twenty bucks.';
+  }
+  else if (ride < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else{
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    return "Thank you so much.";
+  } 
+  else if (tip === "not as generous") {
+    return "Thank you.";
+  }
+  else {
+    return "Bye.";
+  }
 }
